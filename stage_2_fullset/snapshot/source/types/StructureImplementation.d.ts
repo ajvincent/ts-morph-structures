bba4ec81d8db77c7e4c0b8b31fa0a387fe5d1f9e
Alexander J. Vincent <ajvincent@gmail.com>
2023-09-16 13:28:26 -0700

    StructureImplementation, stringOrWriter.

---
+++ stage_2_fullset/snapshot/source/types/StructureImplementation.d.ts
@@ -0,0 +1,47 @@
+import type {
+  Class,
+} from "type-fest";
+
+import type {
+  KindedStructure,
+  Node,
+  OptionalKind,
+  StructureKind,
+} from "ts-morph";
+
+import type {
+  stringOrWriter
+} from "./stringOrWriter.js";
+
+export interface StructureImplementation<
+  Kind extends StructureKind,
+  StructureBase extends KindedStructure<Kind>,
+>
+{
+  readonly kind: Kind;
+
+  leadingTrivia: stringOrWriter[];
+  trailingTrivia: stringOrWriter[];
+
+  /** This is more for debugging purposes than actual code usage. */
+  toJSON(): StructureBase;
+}
+
+export interface NodeWithStructure<Kind extends StructureKind> extends Node {
+  getStructure(): KindedStructure<Kind>
+}
+
+export interface StructureImplementationStatic<
+  Kind extends StructureKind,
+  StructureBase extends KindedStructure<Kind>,
+  NodeBase extends NodeWithStructure<Kind>
+> extends Class<StructureImplementation<Kind, StructureBase>>
+{
+  fromStructure(
+    source: OptionalKind<StructureBase>
+  ): StructureImplementation<Kind, StructureBase>;
+
+  fromNode(
+    source: NodeBase
+  ): StructureImplementation<Kind, StructureBase>;
+}
---
+++ stage_2_fullset/spec-snapshot/source/_types/StructureImplementation.ts
@@ -0,0 +1,54 @@
+import  {
+  JSDocTag,
+  type JSDocTagStructure,
+  type OptionalKind,
+  StructureKind,
+} from "ts-morph";
+
+import type {
+  StructureImplementation,
+  StructureImplementationStatic
+} from "../../../snapshot/source/types/StructureImplementation.js";
+
+import type {
+  stringOrWriter,
+} from "../../../snapshot/source/types/stringOrWriter.js";
+
+class ImplStub implements StructureImplementation<
+  StructureKind.JSDocTag,
+  JSDocTagStructure
+>
+{
+  readonly kind: StructureKind.JSDocTag = StructureKind.JSDocTag;
+  readonly leadingTrivia: stringOrWriter[] = [];
+  readonly trailingTrivia: stringOrWriter[] = [];
+
+  static fromStructure(
+    source: OptionalKind<JSDocTagStructure>
+  ): ImplStub
+  {
+    void(source);
+    throw new Error("not yet implemented");
+  }
+
+  static fromNode(
+    source: JSDocTag
+  ): StructureImplementation<StructureKind.JSDocTag, JSDocTagStructure> {
+    void(source);
+    throw new Error("foo");
+  }
+
+  toJSON(): JSDocTagStructure
+  {
+    throw new Error("Method not implemented.");
+  }
+}
+ImplStub satisfies StructureImplementationStatic<
+  StructureKind.JSDocTag,
+  JSDocTagStructure,
+  JSDocTag
+>;
+
+it("StructureImplementation compiles and is a good base for building structure classes", () => {
+  expect(ImplStub).toBeTruthy();
+});
