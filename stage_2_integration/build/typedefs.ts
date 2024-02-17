57178be852f9a5722a8a655bd627682783c1ad95
Alexander J. Vincent <ajvincent@gmail.com>
2024-02-17 13:15:03 -0800

    #48, include existing type definition files.

--- stage_2_integration/build/typedefs.ts
+++ stage_2_integration/build/typedefs.ts
@@ -6,8 +6,15 @@ import {
 } from "#utilities/source/AsyncSpecModules.js";
 
 import {
+  PromiseAllParallel,
+  PromiseAllSequence,
+} from "#utilities/source/PromiseTypes.js";
+
+import {
   runModule
 } from "#utilities/source/runModule.js";
+
+import readDirsDeep from "#utilities/source/readDirsDeep.js";
 
 import {
   snapshotDir,
@@ -37,6 +44,17 @@ async function compileTypeDefinitions(): Promise<void>
         "--project", tsconfigTarget
       ]
     );
+
+    // the tsc run left behind the .d.ts files we already had
+    let { dirs, files } = await readDirsDeep(snapshotDir);
+    dirs = dirs.map(d => d.replace(snapshotDir, typingsSnapshotDir));
+    files = files.filter(f => f.endsWith(".d.ts"));
+
+    await PromiseAllSequence(dirs, d => fs.mkdir(d, { recursive: true }));
+    await PromiseAllParallel(files, f => {
+      const target = f.replace(snapshotDir, typingsSnapshotDir);
+      return fs.copyFile(f, target)
+    });
   }
   finally {
     await fs.rm(tsconfigTarget);
