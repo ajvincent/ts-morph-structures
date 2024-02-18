36b4c837811c887fb76fed9a5c3c64dbc4f8684c
Alexander J. Vincent <ajvincent@gmail.com>
2024-02-17 16:50:24 -0800

    npm run clean, debugging support

--- utilities/source/recursiveBuild.ts
+++ utilities/source/recursiveBuild.ts
@@ -1,5 +1,7 @@
 import path from "path";
 import { spawn } from "child_process";
+import { env } from "process";
+
 import { Deferred } from "./PromiseTypes.js";
 
 export default async function recursiveBuild(
@@ -9,16 +11,22 @@ export default async function recursiveBuild(
 {
   const d = new Deferred<void>;
 
+  const nodeJSArgs = [
+    /*
+    "../node_modules/ts-node/dist/bin-esm.js",
+    */
+    "--import",
+    "../register-hooks.js",
+    relativePathToModule
+  ];
+
+  if (dirname === env.TSMS_DEBUG) {
+    nodeJSArgs.unshift("--inspect-brk");
+  }
+
   const child = spawn(
     "node",
-    [
-      /*
-      "../node_modules/ts-node/dist/bin-esm.js",
-      */
-      "--import",
-      "../register-hooks.js",
-      relativePathToModule
-    ],
+    nodeJSArgs,
     {
       stdio: ["ignore", "inherit", "inherit", "ipc"],
       cwd: path.join(process.cwd(), dirname)
