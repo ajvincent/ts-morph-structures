b77fe9bbc2d1c91766c773b8a8f3daa18728a9df
Alexander J. Vincent <ajvincent@gmail.com>
2024-02-23 21:05:16 -0800

    #75, filling out interfaces, part 1.

--- stage_3_generation/build/createInterfaces.ts
+++ stage_3_generation/build/createInterfaces.ts
@@ -1,14 +1,13 @@
+import assert from "node:assert/strict";
+
 import {
   StructureKind
 } from "ts-morph";
 
 import {
   getTypeAugmentedStructure,
-  /*
-  LiteralTypeStructureImpl,
+  type PropertySignatureImpl,
   TypeStructureKind,
-  */
-  TypeMembersMap,
   VoidTypeNodeToTypeStructureConsole,
 } from "#stage_two/snapshot/source/exports.js";
 
@@ -18,12 +17,11 @@ import {
 
 import TS_MORPH_D from "#utilities/source/ts-morph-d-file.js";
 
-import AwaitedMap from "#utilities/source/AwaitedMap.js";
-
 import InterfaceModule from "../moduleClasses/InterfaceModule.js";
+import { PromiseAllParallel } from "#utilities/source/PromiseTypes.js";
 
 export default async function createInterfaces(
-  names: readonly string[]
+  structureNames: readonly string[]
 ): Promise<void>
 {
   /* Here's what we have to do.
@@ -34,18 +32,72 @@ export default async function createInterfaces(
   (5) Convert structure members' types to the desired format.
   */
 
-  const decoratorPromiseMap = new AwaitedMap<string, InterfaceModule>;
-  for (const name of names) {
-    createStructureInterface(name, decoratorPromiseMap);
+  const decoratorNamesUnordered: string[] = [];
+  for (const name of structureNames) {
+    createStructureInterface(name);
+    const interfaceModule = InterfaceModule.structuresMap.get(
+      getClassInterfaceName(name)
+    )!
+    decoratorNamesUnordered.push(...interfaceModule.extendsSet);
   }
 
-  const decoratorMap = await decoratorPromiseMap.allResolved();
-  void(decoratorMap);
+  const decoratorNames = new Set<string>(decoratorNamesUnordered);
+  for (const name of decoratorNames) {
+    createDecoratorInterface(name, decoratorNames);
+  }
+
+  const interfaceModules = [
+    ...InterfaceModule.structuresMap.values(),
+    ...InterfaceModule.decoratorsMap.values()
+  ];
+  await PromiseAllParallel(interfaceModules, module => module.saveFile());
 }
 
 function createStructureInterface(
   name: string,
-  decoratorPromiseMap: AwaitedMap<string, InterfaceModule>
+): void
+{
+  const interfaceModule = new InterfaceModule(
+    getClassInterfaceName(name),
+  );
+
+  addInterface(name, interfaceModule);
+  if (name === "SourceFileStructure")
+    interfaceModule.structureKindName = "SourceFile";
+  else
+    assert(interfaceModule.structureKindName, "structure kind name not found for " + name);
+
+  InterfaceModule.structuresMap.set(
+    interfaceModule.defaultExportName, interfaceModule
+  );
+
+  tightenTypeMembers(interfaceModule);
+}
+
+function createDecoratorInterface(
+  name: string,
+  decoratorNames: Set<string>,
+): void
+{
+  const interfaceModule = new InterfaceModule(
+    getClassInterfaceName(name),
+  );
+
+  addInterface(name, interfaceModule);
+  interfaceModule.extendsSet.forEach(
+    extendName => decoratorNames.add(extendName)
+  );
+
+  InterfaceModule.decoratorsMap.set(
+    interfaceModule.defaultExportName, interfaceModule
+  );
+
+  tightenTypeMembers(interfaceModule);
+}
+
+function addInterface(
+  name: string,
+  interfaceModule: InterfaceModule,
 ): void
 {
   const structureInterface = getTypeAugmentedStructure(
@@ -55,14 +107,47 @@ function createStructureInterface(
     StructureKind.Interface
   ).rootStructure;
 
-  const interfaceModule = new InterfaceModule(
-    getClassInterfaceName(name),
-    TypeMembersMap.fromMemberedObject(structureInterface),
-    structureInterface.extendsSet
-  );
-  InterfaceModule.structuresMap.set(
-    interfaceModule.defaultExportName, interfaceModule
-  );
+  for (const extendsValue of structureInterface.extendsSet) {
+    const { kind } = extendsValue;
+    assert(kind === TypeStructureKind.Literal || kind === TypeStructureKind.TypeArgumented);
+    if (kind === TypeStructureKind.Literal) {
+      const id = extendsValue.stringValue;
+      if (id.endsWith("SpecificStructure") || id.endsWith("BaseStructure")) {
+        addInterface(id, interfaceModule);
+      } else {
+        interfaceModule.extendsSet.add(extendsValue.stringValue);
+      }
+    }
+    else {
+      assert.equal(extendsValue.objectType.kind, TypeStructureKind.Literal);
+      assert.equal(extendsValue.objectType.stringValue, "KindedStructure");
 
-  void(decoratorPromiseMap);
+      const kindedName = extendsValue.childTypes[0];
+      assert.equal(kindedName.kind, TypeStructureKind.QualifiedName);
+      assert.equal(kindedName.childTypes[0], "StructureKind");
+      assert.equal(kindedName.childTypes.length, 2);
+
+      interfaceModule.structureKindName = kindedName.childTypes[1];
+    }
+  }
+
+  interfaceModule.typeMembers.addMembers(structureInterface.properties);
+}
+
+function tightenTypeMembers(
+  interfaceModule: InterfaceModule
+): void
+{
+  interfaceModule.typeMembers.arrayOfKind(StructureKind.PropertySignature).forEach(
+    property => tightenProperty(interfaceModule, property)
+  );
+}
+
+function tightenProperty(
+  interfaceModule: InterfaceModule,
+  property: PropertySignatureImpl,
+): void
+{
+  void(interfaceModule); // allows setting imports
+  void(property);
 }
--- stage_3_generation/moduleClasses/InterfaceModule.ts
+++ stage_3_generation/moduleClasses/InterfaceModule.ts
@@ -1,7 +1,8 @@
 import {
+  InterfaceDeclarationImpl,
   SourceFileImpl,
+  type TypeMemberImpl,
   TypeMembersMap,
-  type TypeStructures
 } from "#stage_two/snapshot/source/exports.js";
 
 import BaseModule from "./BaseModule.js";
@@ -11,21 +12,45 @@ class InterfaceModule extends BaseModule {
   static readonly decoratorsMap = new Map<string, InterfaceModule>;
   static readonly structuresMap = new Map<string, InterfaceModule>;
 
+  static #entryComparator(
+    this: void,
+    a: [string, TypeMemberImpl],
+    b: [string, TypeMemberImpl]
+  ): number
+  {
+    if (a[0] === "kind")
+      return -1;
+    if (b[0] === "kind")
+      return +1;
+  
+    return a[0].localeCompare(b[0]);
+  }
+
   readonly typeMembers: TypeMembersMap;
-  readonly extendsSet: Set<TypeStructures>;
+  readonly extendsSet: Set<string>;
+
+  structureKindName?: string;
 
   constructor(
     interfaceName: string,
-    typeMembers: TypeMembersMap,
-    extendsSet: Set<TypeStructures>,
   )
   {
     super("source/interfaces/standard", interfaceName, true);
-    this.typeMembers = typeMembers;
-    this.extendsSet = extendsSet;
+    this.typeMembers = new TypeMembersMap;
+    this.extendsSet = new Set<string>;
   }
 
   protected getSourceFileImpl(): SourceFileImpl {
-    throw new Error("Method not implemented.");
+    const sourceFile = new SourceFileImpl;
+
+    const interfaceDecl = new InterfaceDeclarationImpl(this.defaultExportName);
+    interfaceDecl.isExported = true;
+    this.typeMembers.sortEntries(InterfaceModule.#entryComparator);
+    this.typeMembers.moveMembersToType(interfaceDecl);
+    sourceFile.statements.push(
+      interfaceDecl
+    );
+
+    return sourceFile;
   }
 }
